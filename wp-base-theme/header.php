a6acf5c8280a2f433d487d7f57c93589f24e44aa
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-08-09 15:14:13 +0200

    Created auto load of styles

--- wp-base-theme/header.php
+++ wp-base-theme/header.php
@@ -10,57 +10,60 @@
  */
 
 ?>
-<!doctype html>
+    <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="description" charset="<?php bloginfo( 'description' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="icon" type="image/png"
-          href="<?php echo get_static("images/favicon.png") ?>">
-	<?php wp_head(); ?>
-</head>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="description" charset="<?php bloginfo('description'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="https://gmpg.org/xfn/11">
+        <link rel="icon" type="image/png"
+              href="<?php echo get_static("images/favicon.png") ?>">
+        <?php wp_head(); ?>
+    </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp-base-theme' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'wp-base-theme'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$wp_base_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $wp_base_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $wp_base_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header">
+        <div class="site-branding">
+            <?php
+            the_custom_logo();
+            if (is_front_page() && is_home()) :
+                ?>
+                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php
+            else :
+                ?>
+                <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                         rel="home"><?php bloginfo('name'); ?></a></p>
+            <?php
+            endif;
+            $wp_base_theme_description = get_bloginfo('description', 'display');
+            if ($wp_base_theme_description || is_customize_preview()) :
+                ?>
+                <p class="site-description"><?php echo $wp_base_theme_description; /* WPCS: xss ok. */ ?></p>
+            <?php endif; ?>
+        </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wp-base-theme' ); ?></button>
+        <nav id="site-navigation" class="main-navigation">
+            <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php esc_html_e('Primary Menu', 'wp-base-theme'); ?></button>
             <?php /* menu */
-            wp_nav_menu( array(
-                    'menu'           => 'menu-1',
+            wp_nav_menu(array(
+                    'menu' => 'menu-1',
                     'theme_location' => 'menu-1',
-                    'depth'          => 5,
-                    'container'      => false,
-                    'menu_class'     => 'navbar-nav ml-auto',
-                    'fallback_cb'    => 'wp_bootstrap_navwalker::fallback',
-                    'walker'         => new wp_bootstrap_navwalker()
+                    'depth' => 5,
+                    'container' => false,
+                    'menu_class' => 'navbar-nav ml-auto',
+                    'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+                    'walker' => new wp_bootstrap_navwalker()
                 )
             );
             ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
--- wp-base-theme/inc/template/template-functions.php
+++ wp-base-theme/inc/template/template-functions.php
@@ -11,27 +11,59 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function wp_base_theme_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
+function wp_base_theme_body_classes($classes) {
+    // Adds a class of hfeed to non-singular pages.
+    if (!is_singular()) {
+        $classes[] = 'hfeed';
+    }
 
-	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
-	}
+    // Adds a class of no-sidebar when there is no sidebar present.
+    if (!is_active_sidebar('sidebar-1')) {
+        $classes[] = 'no-sidebar';
+    }
 
-	return $classes;
+    return $classes;
 }
-add_filter( 'body_class', 'wp_base_theme_body_classes' );
+
+add_filter('body_class', 'wp_base_theme_body_classes');
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
 function wp_base_theme_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
-	}
+    if (is_singular() && pings_open()) {
+        printf('<link rel="pingback" href="%s">', esc_url(get_bloginfo('pingback_url')));
+    }
 }
-add_action( 'wp_head', 'wp_base_theme_pingback_header' );
+
+add_action('wp_head', 'wp_base_theme_pingback_header');
+
+
+/**
+ * Get the last compiled file from the static/assets folder.
+ * @param string $path_to_style
+ * @return array
+ */
+function get_compiled_style($path_to_style = '/static/assets/') {
+    $url = get_template_directory() . '/webpack-bundle.json';
+    $json_string = file_get_contents($url);
+    $json = json_decode($json_string, true);
+    $styles = ['js' => [], 'css' => []];
+    foreach ($json['chunks']['storefront'] as $style) {
+        $path = get_template_directory_uri() . $path_to_style . $style['name'];
+        $pathinfo = pathinfo($path);
+        $ext = $pathinfo['extension'];
+        if (isset($styles[$ext])) {
+            array_push($styles[$ext], array(
+                'basename' => $pathinfo['basename'],
+                'path' => $path
+            ));
+        } else {
+            $styles[$ext] = array(
+                'basename' => pathinfo($path, PATHINFO_BASENAME),
+                'path' => $path
+            );
+        }
+    }
+    return $styles;
+}
